f21b4af6906462fecda2edf84fa3b53d6c26379b
MangoYX <yousef.w.awad@gmail.com>
2026-07-23 14:53:41 +0300

    added persistence feature

--- lib/task.dart
+++ lib/task.dart
@@ -3,4 +3,14 @@ class Task {
   bool isDone = false;
 
   Task(this.title);
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'isDone': isDone,
+    };
+}
+
+Task.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        isDone = json['isDone'];
 }
